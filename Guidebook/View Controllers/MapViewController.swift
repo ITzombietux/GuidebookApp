4c6ecea890857eb7e14722d4fb8c3c7aac7c59c3
zombietux <zombietux@Macui-MacBook-Pro-2.local>
2019-02-23 14:47:18 +0900

    Geolocation

--- Guidebook/View Controllers/MapViewController.swift
+++ Guidebook/View Controllers/MapViewController.swift
@@ -13,16 +13,30 @@ class MapViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
     
+    var locationManager:CLLocationManager?
+    var lastKnowLocation:CLLocation?
+    
     var place: Place?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Show the blue dot for the user if the location is known
+        mapView.showsUserLocation = true
+        
+        // Create and configure the location manager
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+    
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
         // Check for a place and plot the pin
         if place != nil {
             plotPin(place!)
         }
-
     }
     
     func plotPin(_ p: Place) {
@@ -41,6 +55,31 @@ class MapViewController: UIViewController {
         
     }
     
+    func showGeolocationError() {
+        
+        // Create the error alert
+        let alert = UIAlertController(title: "Geolocation failed", message: "Location services are turned off or this app doesn't have permission to geolocate", preferredStyle: .alert)
+        
+        // Create the settings button
+        let settingsAction = UIAlertAction(title: "Settings", style: .default) { (alert) in
+            
+            let url = URL(string: UIApplication.openSettingsURLString)
+            
+            if let url = url {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        alert.addAction(settingsAction)
+        
+        // Create the cancel button
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addAction(cancelAction)
+        
+        // Show the alert
+       present(alert, animated: true, completion: nil)
+    }
+    
     @IBAction func backTapped(_ sender: UIButton) {
         
         // Dismiss the map view controller
@@ -48,6 +87,38 @@ class MapViewController: UIViewController {
     }
     
     @IBAction func locateTapped(_ sender: UIButton) {
+        
+        if CLLocationManager.locationServicesEnabled() {
+            
+            // check the authorization status
+            let status = CLLocationManager.authorizationStatus()
+            
+            if status == .denied || status == .restricted {
+                
+                // Show the error popup
+                showGeolocationError()
+                
+            } else if status == .authorizedWhenInUse || status == .authorizedAlways {
+                
+                // Start geolocating the user
+                locationManager?.startUpdatingLocation()
+                
+                // Center the map around the last known location
+                if let lastKnownLocation = lastKnowLocation {
+                    mapView.setCenter(lastKnownLocation.coordinate, animated: true)
+                }
+                
+            } else if status == .notDetermined {
+                
+                // Ask the user foe permission
+                locationManager?.requestAlwaysAuthorization()
+            }
+            
+        } else {
+            
+            // Location services turned off
+            showGeolocationError()
+        }
     }
     
     @IBAction func routeTapped(_ sender: UIButton) {
@@ -68,3 +139,34 @@ class MapViewController: UIViewController {
     }
 }
 
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations.last
+        
+        if let location = location {
+            
+            // Center the map around this location, only if it's the first time location the user
+            if lastKnowLocation == nil {
+            mapView.setCenter(location.coordinate, animated: true)
+            }
+            
+            lastKnowLocation = location
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status == .denied || status == .restricted {
+            
+            // User chose don't allow
+            showGeolocationError()
+        } else if status == .authorizedAlways || status == .authorizedWhenInUse {
+            
+            // Start locating the user
+            locationManager?.startUpdatingLocation()
+            
+        }
+    }
+}
